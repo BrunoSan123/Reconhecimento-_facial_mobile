cb28f967b260e48b004a085ce9cd65ed6c66d8cb
Bruno Barreto Alves da Silva <Bruno-kk@hotmail.com>
2020-06-26 16:29:50 -0300

    Bug da renderização simultanea,consertado

--- Facial/app/src/main/java/com/example/facial/MainActivity.java
+++ Facial/app/src/main/java/com/example/facial/MainActivity.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ea1234d7219810837be0513b4a1435e35c87062e17c0a61e8ceba2c41156727e
-size 5797
+oid sha256:e0f1f0ec23efcd58c7df6e0492183964fbdbcf401792ed9c2bdf49ab9d19c679
+size 6059
